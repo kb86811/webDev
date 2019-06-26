13749b3d0608381962d52ae733e74440e5b962f8
Kedir Birhan <kedirbirhan@yahoo.com>
2019-06-26 16:03:08 -0400

    continued from earlier

--- JS/project1/02script.js
+++ JS/project1/02script.js
@@ -9,16 +9,16 @@
 // the buttons should follow the same rules with user inputed score points to win the game.
 //****************************************************************************/
 // the Reset button 
-// the reset button will reset the score for bothn  to 0.
+// the reset button will reset the score for both  to 0.
 
 //*************************************************************************** */
 
 //thses two selects the player one and playe two buttons
-var p1button=document.querySelector("#p1");
-var p2button=document.getElementById("p2");
+var p1button=document.querySelector("#p1"); //select player one button 
+var p2button=document.getElementById("p2"); //select player two button, using diffrent kind of selector for practices
 var p1Score=0;
 var p2Score=0;
-//selecting the score for player 1. we have added a span with id to the numerical parts of the h1 tag so we can select and update score as the user cliks the buttons. 
+//selecting the score for player 1. we have added a span with id to the numerical parts of the h1 tag so we can select and update score as the user clicks the buttons. 
 //<h1><span id="p1Display">0</span> to <span id="p2Display">0</span></h1>
 var p1Display=document.querySelector("#p1Display");
 //selecting the score for player 2
@@ -33,7 +33,6 @@ var winningScore=5;
 //the buttons should not increment when one of the player reaches the winning score.
 //when one of the player reaches winning score, the color of the number that shows current score should chnage to green for the winner player only.
 p1button.addEventListener("click",function(){
-
     if(!gameOver){
         p1Score++;
         if(p1Score === winningScore){
@@ -60,7 +59,7 @@ p2button.addEventListener("click",function(){
 });
 //when the reset button is clickecd, we want to rest the p1Score and p2Score vraible to 0.
 // and we want to rest the score board on the html to 0 as well
-// and we want to remove the green class that is set in case it was clicked after a winning score
+// and we want to remove the green class that could be added to one of the two players score counter in the html,  in case it was clicked after a winning score
 //lastly we want to make sure that the boolen gameOver is set to false so the code can run again
 var resetButton=document.getElementById("reset");
 
@@ -69,33 +68,43 @@ resetButton.addEventListener("click",function(){
     reset();
 });
 
-//********************reset ****************************
+//********************input field ****************************
  //now when the input field is set to the number entered we want the "playing to field to change to that number" 
  // so we jsut have to adjust the winning score to the user inputed number.
 
  //here we are selecting the input field
  var numInput=document.querySelector("input");
-//here we are selctingthe spand inside of the p tag
+//here we are selectingthe span inside of the p tag
  var winnningScoreDisplay=document.querySelector("p span");
+ // we are using the change since the input box can be changed by not only a click but also, a user can input the games round through keyboard and we want to detect that too. so we are using change event listener to detect chnages in the input and respond
  numInput.addEventListener("change", function(){
     //  var input =document.querySelector("input");
     // console.log(input);
     // p.textContent="value chnaged"
     // winnningScoreDisplay.textContent=numInput.value;
+    //** here this keyword refers to the numInput element, so below we are getting the value inputed in the input box, using this.value and updating the round displayer to that value */
     winnningScoreDisplay.textContent=this.value;
 
     // winningScore=Number(numInput.value);
+    // since  this.value returns string representation of the numebr, we need to cast it to a number so, later when we compare its value, it wont cause a bug.
+    // if(p1Score === winningScore){ // this is come from the above codes where we are using winningScore to com to p1Score, if we hadnt changed here to a number we would have been comparing a string and an int
     winningScore=Number(this.value);
 
     reset();
 
  });
+ //********************reset button  ****************************
 
+//when the reset button is clickecd, we want to rest the p1Score and p2Score vraible to 0.
+// and we want to rest the score board on the html to 0 as well
+// and we want to remove the green class that could be added to one of the two players score counter in the html,  in case it was clicked after a winning score
+//lastly we want to make sure that the boolen gameOver is set to false so the code can run again
  function reset(){
     p1Score=0;
     p2Score=0;
     p1Display.textContent="0";
     p2Display.textContent="0";
+    //we are not gonna check which span is has the green clas turned on, we will just remove it from both
     p1Display.classList.remove("winner");
     p2Display.classList.remove("winner");
     gameOver=false;
--- JS/project2/03todos.js
+++ JS/project2/03todos.js
@@ -1,12 +1,12 @@
 //slects the first li  -- <li>Wash Cat</li>
 var lis = document.querySelectorAll("li");
-
+// in ths for loop we are adding three event listener
 for(var i = 0; i < lis.length; i++){
 	lis[i].addEventListener("mouseover", function(){
 		// this.style.color="green"         //<-- this works as well but we dont want 
 		this.classList.add("selected");
 	});
-
+// when you move the mouse away from the li, this listener recognizes the mouse has been moved from the li and runs the callback function
 	lis[i].addEventListener("mouseout", function(){
 		this.classList.remove("selected");
 	});
